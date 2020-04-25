1d7a12ac2b2c9cf9ef0f676929bccccf7a5c6451
Michael Lipp <mnl@mnl.de>
2020-04-25 23:16:06 +0200

    Continue test.

--- test/i2c-virt-bus-test/EepromTest.h
+++ test/i2c-virt-bus-test/EepromTest.h
@@ -10,6 +10,7 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 #include <cppunit/TestFixture.h>
@@ -29,9 +30,13 @@ private:
 
 public:
 	void setUp() {
-		eepromDev = open(STR(I2C_BUS), O_RDWR);
-		CPPUNIT_ASSERT_MESSAGE("Cannot open i2c bus " STR(I2C_BUS),
-				eepromDev >= 0);
+		char* i2cBus = getenv("I2C_BUS");
+		CPPUNIT_ASSERT_MESSAGE("I2C_BUS not set in environment",
+				i2cBus != nullptr);
+		eepromDev = open(i2cBus, O_RDWR);
+		std::ostringstream msg;
+		msg << "Cannot open i2c bus " << i2cBus;
+		CPPUNIT_ASSERT_MESSAGE(msg.str(), eepromDev >= 0);
 		int res = ioctl(eepromDev, I2C_SLAVE, EEPROM_ADDR);
 		CPPUNIT_ASSERT_MESSAGE(
 				"Failed to acquire bus access and/or talk to slave", res >= 0);
@@ -43,12 +48,21 @@ public:
 
 	void testSimpleRW() {
 		char out[] = { 0, 0x42, 0x12, 0x34 };
+		// write two bytes
 		int res = write(eepromDev, out, 4);
-		CPPUNIT_ASSERT_MESSAGE("Failed to write data", res != 4);
+		CPPUNIT_ASSERT_MESSAGE("Failed to write data", res == 4);
+		// write address for reading back
+		res = write(eepromDev, out, 2);
+		CPPUNIT_ASSERT_MESSAGE("Failed to write data", res == 2);
+		// read back
+		char in[2];
+		res = read(eepromDev, in, 2);
+		CPPUNIT_ASSERT_MESSAGE("Failed to read data", res == 2);
+		CPPUNIT_ASSERT(in[0] == 0x12);
+		CPPUNIT_ASSERT(in[1] == 0x34);
 	}
 
 	void testMultipleRW() {
-		int i = 0;
 	}
 
 };
